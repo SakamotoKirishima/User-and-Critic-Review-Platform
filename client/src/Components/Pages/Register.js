a680edf492f542b938665cc38ffca8312ce20722
The-curs0r <6ak756yti@gmail.com>
2020-02-18 19:29:31 +0530

    First commit

---
+++ client/src/Components/Pages/Register.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+// import logo from './logo.svg';
+import PropTypes from 'prop-types';
+class Register extends Component {
+    render() {
+        return (
+            <div className="RegPage">
+                wassa
+            </div>
+        );
+    }
+}
+
+export default Register;
